bb293a32271506e75c230e566c8498b222e3f40f
michaelwestblade <michaelwestblade@gmail.com>
2025-05-02 14:04:32 -0500

    accessing s3 from a lambda

--- space-finder/src/infra/stacks/LambdaStack.ts
+++ space-finder/src/infra/stacks/LambdaStack.ts
@@ -1,13 +1,14 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-import { Code, Runtime } from 'aws-cdk-lib/aws-lambda';
+import { Runtime } from 'aws-cdk-lib/aws-lambda';
 import { join } from 'path';
 import { LambdaIntegration } from 'aws-cdk-lib/aws-apigateway';
 import { ITable } from 'aws-cdk-lib/aws-dynamodb';
 import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
+import { Effect, PolicyStatement } from 'aws-cdk-lib/aws-iam';
 
 interface LambdaStackProps extends StackProps {
-  spacesTable: ITable
+  spacesTable: ITable;
 }
 
 export class LambdaStack extends Stack {
@@ -24,6 +25,14 @@ export class LambdaStack extends Stack {
         TABLE_NAME: props.spacesTable.tableName,
       },
     });
+
+    helloLambda.addToRolePolicy(
+      new PolicyStatement({
+        effect: Effect.ALLOW,
+        actions: ['s3:ListAllMyBuckets', 's3:ListBuckets'],
+        resources: ['*'],
+      }),
+    );
 
     this.helloLambdaIntegration = new LambdaIntegration(helloLambda);
   }
--- space-finder/src/services/hello.ts
+++ space-finder/src/services/hello.ts
@@ -1,9 +1,13 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult, Context } from 'aws-lambda';
 import {v4 as uuidv4} from 'uuid';
+import {S3Client, ListBucketsCommand} from '@aws-sdk/client-s3';
+
+const s3 = new S3Client({});
 
 async function handler(event: APIGatewayProxyEvent, context: Context): Promise<APIGatewayProxyResult> {
-  console.log('event',event);
-  console.log('context', context);
+  const command = new ListBucketsCommand({});
+  const listBucketsResult = await s3.send(command);
+  console.log(listBucketsResult);
 
   const uuid = uuidv4();
 
