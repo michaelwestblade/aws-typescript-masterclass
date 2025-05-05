6191d3872fe8b3008310a3e0e3d5862ca3b7ed63
michaelwestblade <michaelwestblade@gmail.com>
2025-05-05 11:29:42 -0500

    get by id

--- space-finder/src/services/spaces/GetSpaces.ts
+++ space-finder/src/services/spaces/GetSpaces.ts
@@ -3,14 +3,38 @@ import {
   APIGatewayProxyResult,
   Context,
 } from 'aws-lambda';
-import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
+import {
+  DynamoDBClient,
+  GetItemCommand,
+  ScanCommand,
+} from '@aws-sdk/client-dynamodb';
 
 export async function GetSpaces(
   event: APIGatewayProxyEvent,
   dynamoClient: DynamoDBClient,
 ): Promise<APIGatewayProxyResult> {
+  if (event.queryStringParameters?.id) {
+    const result = await dynamoClient.send(
+      new GetItemCommand({
+        TableName: process.env.TABLE_NAME || '',
+        Key: { id: { S: event.queryStringParameters.id } },
+      }),
+    );
+
+    return {
+      statusCode: result.Item ? 200 : 404,
+      body: JSON.stringify({ result }),
+    };
+  }
+
+  const result = await dynamoClient.send(
+    new ScanCommand({
+      TableName: process.env.TABLE_NAME || '',
+    }),
+  );
+
   return {
     statusCode: 200,
-    body: JSON.stringify({ message: 'Hello from GET' }),
+    body: JSON.stringify({ result }),
   };
 }
