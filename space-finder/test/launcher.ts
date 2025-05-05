602f3f342632ad924ac72e140268c884a774fa98
michaelwestblade <michaelwestblade@gmail.com>
2025-05-05 11:05:01 -0500

    updating testing function

--- space-finder/test/launcher.ts
+++ space-finder/test/launcher.ts
@@ -6,64 +6,6 @@ handler(
     headers: {},
     multiValueHeaders: {},
     httpMethod: 'POST',
-    isBase64Encoded: false,
-    path: '',
-    pathParameters: null,
-    queryStringParameters: null,
-    multiValueQueryStringParameters: null,
-    stageVariables: null,
-    requestContext: {
-      accountId: '',
-      apiId: '',
-      authorizer: undefined,
-      protocol: '',
-      httpMethod: '',
-      identity: {
-        accessKey: null,
-        accountId: null,
-        apiKey: null,
-        apiKeyId: null,
-        caller: null,
-        clientCert: null,
-        cognitoAuthenticationProvider: null,
-        cognitoAuthenticationType: null,
-        cognitoIdentityId: null,
-        cognitoIdentityPoolId: null,
-        principalOrgId: null,
-        sourceIp: '',
-        user: null,
-        userAgent: null,
-        userArn: null,
-      },
-      path: '',
-      stage: '',
-      requestId: '',
-      requestTimeEpoch: 0,
-      resourceId: '',
-      resourcePath: '',
-    },
-    resource: '',
-  },
-  {
-    callbackWaitsForEmptyEventLoop: false,
-    functionName: '',
-    functionVersion: '',
-    invokedFunctionArn: '',
-    memoryLimitInMB: '',
-    awsRequestId: '',
-    logGroupName: '',
-    logStreamName: '',
-    getRemainingTimeInMillis: function (): number {
-      throw new Error('Function not implemented.');
-    },
-    done: function (error?: Error, result?: any): void {
-      throw new Error('Function not implemented.');
-    },
-    fail: function (error: Error | string): void {
-      throw new Error('Function not implemented.');
-    },
-    succeed: function (messageOrObject: any): void {
-      throw new Error('Function not implemented.');
-    },
-  },
+  } as any,
+  {} as any,
 );
